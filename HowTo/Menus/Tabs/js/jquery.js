4d76f960737ea3c26d82f2bc1c487c923da1f387
bgkoh86 <bgkoh86@hanmail.net>
2020-10-30 21:29:43 +0900

    add : vertical tabs

--- HowTo/Menus/Tabs/js/jquery.js
+++ HowTo/Menus/Tabs/js/jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // variables
+    // Horizon tabs
     $(".tabs__link").click(function(e) {
     let currentLink = $(this).text();
         e.preventDefault();
@@ -12,5 +12,21 @@ $(document).ready(function() {
     $(".tabs__close").click(function() {
         $(this).parent().hide();
         $(".tabs__link").removeClass("active");
+    })
+
+    // Vertical tabs
+    $(".vertical-tabs__link").click(function(e) {
+        e.preventDefault();
+        $(".vertical-tabs__link").removeClass("active");
+        $(this).addClass("active");
+
+        let linkText = $(this).text();
+        $(".vertical-tabs__content").hide();
+        $(`.vertical-tabs__content--${linkText}`).show();
+    })
+
+    $(".vertical-tabs__close").click(function() {
+        $(this).parent().hide();
+        $(".vertical-tabs__link").removeClass("active");
     })
 });
