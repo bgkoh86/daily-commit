f7f98100ad4b42db2bc92af5c3447de8a1db7d6f
bgkoh86 <bgkoh86@hanmail.net>
2020-11-02 00:28:39 +0900

    add : Auto-slideshow

---
+++ HowTo/Images/1-1. Auto-Slideshow/js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    let slideIndex = 0;
+    showSlides();
+
+    
+    // showSlide logic
+    function showSlides() {
+        console.log(slideIndex);
+        let imgs = $(".slideshow__box");
+        let currentImg = imgs[slideIndex];
+        let dots = $(".slideshow__dot");
+        let currentDot = dots[slideIndex];
+    
+        imgs.hide();
+        $(currentImg).show();
+
+        dots.removeClass("active-dot");
+        $(currentDot).addClass("active-dot");
+
+        slideIndex++;
+
+        if (slideIndex == imgs.length) {
+            slideIndex = 0;
+        }
+
+    setTimeout(showSlides, 2000);
+
+    };
+
+    // dot click
+    $(".slideshow__dot").click(function() { 
+        var imgs = $(".slideshow__box");  // array
+        var dotIndex = $(this).index();  // index
+        var selectedImg = imgs[dotIndex]; 
+
+
+        imgs.hide();
+        $(selectedImg).show();
+
+        $(".slideshow__dot").removeClass("active-dot");
+        $(this).addClass("active-dot");
+        })
+});
