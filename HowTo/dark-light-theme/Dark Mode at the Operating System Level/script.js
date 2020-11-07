fe0f465600a352ed76cdb8f5581a6b368118158d
bgkoh86 <bgkoh86@hanmail.net>
2020-11-07 23:19:21 +0900

    add : localStorage saved dark-mode

--- HowTo/dark-light-theme/Dark Mode at the Operating System Level/script.js
+++ HowTo/dark-light-theme/Dark Mode at the Operating System Level/script.js
@@ -1,12 +1,26 @@
 const btn = document.querySelector(".btn-toggle");
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-console.log(prefersDarkScheme.matches);
+if (localStorage.theme == "dark") {
+  document.body.classList.add("dark-theme");
+} else {
+  document.body.classList.add("light-theme");
+}
 
-btn.addEventListener("click", function () {
-  if (prefersDarkScheme.matches) {
+btn.addEventListener("click", function() {
+  if (matchMedia("(prefers-color-scheme: dark").matches) {
     document.body.classList.toggle("light-theme");
+    if (document.body.classList.contains("light-theme")) {
+      localStorage.setItem("theme", "light");
+    } else {
+      localStorage.setItem("theme", "dark");
+    }
   } else {
     document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
   }
-});
+})
+
