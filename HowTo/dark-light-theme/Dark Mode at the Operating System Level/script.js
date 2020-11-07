fda7b91d047e44445ddc3d7d5dd1ddf727be1f1c
bgkoh86 <bgkoh86@hanmail.net>
2020-11-08 07:04:48 +0900

    modified : dark-mode at the operating system level

--- HowTo/dark-light-theme/Dark Mode at the Operating System Level/script.js
+++ HowTo/dark-light-theme/Dark Mode at the Operating System Level/script.js
@@ -1,26 +1,9 @@
 const btn = document.querySelector(".btn-toggle");
 
-if (localStorage.theme == "dark") {
-  document.body.classList.add("dark-theme");
-} else {
-  document.body.classList.add("light-theme");
-}
-
 btn.addEventListener("click", function() {
   if (matchMedia("(prefers-color-scheme: dark").matches) {
     document.body.classList.toggle("light-theme");
-    if (document.body.classList.contains("light-theme")) {
-      localStorage.setItem("theme", "light");
-    } else {
-      localStorage.setItem("theme", "dark");
-    }
   } else {
     document.body.classList.toggle("dark-theme");
-    if (document.body.classList.contains("dark-theme")) {
-      localStorage.setItem("theme", "dark");
-    } else {
-      localStorage.setItem("theme", "light");
-    }
   }
-})
-
+});
